97ae6dee223132caea3ca51f55417af5225fb840
Albin Ohlsson <albin_ohlsson@hotmail.com>
2017-10-13 19:40:49 +0200

    implemented getProbs, seems to be right

--- findcroc.R
+++ findcroc.R
@@ -1,24 +1,29 @@
 bestWC=function(moveInfo,readings,positions,edges,probs) {
-  state = getState()
+  moveInfo$mem = getState(moveInfo, positions, readings, probs)
   crocAt = max(state) #or however you get the index of the highest value in a vector
   direction = search(currentPosition, crocAt);
   moveInfo$moves=c(direction,0)
   return(moveInfo)
 }
 
-getState=function(moveInfo, positions){
+getState=function(moveInfo, positions, edges, readings, probs){
   #first round
   if(length(moveInfo$mem) == 0){
     moveInfo$mem = list(Tmat = getTmat(edges), lastState = matrix(1/40, nrow = 1, ncol = 40))
+    return (moveInfo$mem)
   }
+  #backpacker died
   for (i in seq_len(2)){
     if (!is.na(positions[i]) && positions[i] < 0){
       state = matrix(0, nrow = 1, ncol = 40)
       state[-positions[i]] = 1
-      return (state)
+      moveInfo$mem$lastState = state
+      return (moveInfo$mem)
     }
   }
-  state = moveInfo$mem$lastState*moveInfo$mem$Tmat
+  state = moveInfo$mem$lastState %*% moveInfo$mem$Tmat
+  probs = getProbs(readings, probs)
+  state = state * probs
   
 }
 
@@ -42,7 +47,17 @@ getTmat=function(edges){
   return (Tmat)
 }
 
-getProbs=function(){
+getProbs=function(readings, probs){
   #use dnorm(reading, mean, stdev) for each positions salinity, phosphate and nitrogen
   #should return a vector of probabilities the croc is in each hole given the readings
+  probVec = matrix(0, nrow = 40, ncol = 1)
+  for (i in seq_len(40)){
+    probElem = dnorm(readings[1], probs$salinity[i,1], probs$salinity[i,2])
+    probElem = probElem * dnorm(readings[2], probs$phosphate[i,1], probs$phosphate[i,2])
+    probElem = probElem * dnorm(readings[3], probs$nitrogen[i,1], probs$nitrogen[i,2])
+    probVec[i] = probElem
+    sumProbs = sum(probVec)
+    probVecNormalized = probVec/sumProbs
+  }
+  return (probVecNormalized)
 }
