16f6ba82e0db1dfc1fc726ce38f53ee5ac99617f
Albin Ohlsson <albin_ohlsson@hotmail.com>
2017-10-14 13:56:15 +0200

    seems to work now

--- findcroc.R
+++ findcroc.R
@@ -1,12 +1,12 @@
 bestWC=function(moveInfo,readings,positions,edges,probs) {
-  moveInfo$mem = getState(moveInfo, positions, readings, probs)
-  crocAt = max(state) #or however you get the index of the highest value in a vector
-  direction = search(currentPosition, crocAt);
-  moveInfo$moves=c(direction,0)
+  moveInfo$mem = getState(moveInfo,readings,positions,edges,probs)
+  crocAt = match(max(moveInfo$mem$lastState),moveInfo$mem$lastState) 
+  direction = search(positions[3], crocAt, edges)
+  moveInfo$moves=c(direction, 0)
   return(moveInfo)
 }
 
-getState=function(moveInfo, positions, edges, readings, probs){
+getState=function(moveInfo,readings,positions,edges,probs){
   #first round
   if(length(moveInfo$mem) == 0){
     moveInfo$mem = list(Tmat = getTmat(edges), lastState = matrix(1/40, nrow = 1, ncol = 40))
@@ -22,15 +22,75 @@ getState=function(moveInfo, positions, edges, readings, probs){
     }
   }
   state = moveInfo$mem$lastState %*% moveInfo$mem$Tmat
-  probs = getProbs(readings, probs)
-  state = state * probs
-  
+  probbies = getProbs(readings, probs)
+  state = t(t(state) * probbies)
+  sumState = sum(state)
+  stateNormalized = state/sumState
+  moveInfo$mem$lastState = stateNormalized
+  return (moveInfo$mem)
 }
 
-search=function(currentPosition, crocAt){
+search=function(currentPosition, crocAt, edges){
   #calculate best path, return a direction to go
   #probably dijsktra, since we have no heuristic for Astar
   #or greedy hill climb or something
+  visited = list()
+  frontier = list(list(pos = currentPosition, cost = 0, prev = NULL))
+  while(indexOf(visited, crocAt) == -1){
+    bestindex = best(frontier)
+    current = frontier[[bestindex]]
+    frontier[[bestindex]] = NULL
+    frontier = addFrontier(current, visited, frontier,edges)
+    visited[[length(visited)+1]] = current
+  }
+  end = visited[[length(visited)]]
+  while(!is.null(end$prev$prev)){
+    end = end$prev
+  }
+  return(end$pos)
+}
+
+best=function(frontier){
+  best1 = 1
+  min = 100000;
+  for (i in seq_len(length(frontier)))
+  {
+    if (frontier[[i]]$cost < min){
+      min = frontier[[i]]$cost
+      best1 = i
+    }
+  }
+  return (best1)
+}
+#takes a list of nodes (pos, cost, prev) and a pos (int) and returns the index of it, or -1
+indexOf=function(list, current){
+  for(i in seq_len(length(list))){
+    if (!is.na(list[[i]]$pos == current)){
+      if (list[[i]]$pos == current){
+        return (i)
+      }
+    }
+  }
+  return (-1)
+}
+
+addFrontier=function(current, visited, frontier,edges){
+  options = getOptions(current$pos, edges)
+  for (option in options){
+    if (indexOf(visited, option) == -1){
+      indexinfrontier = indexOf(frontier, option)
+      if(indexinfrontier != -1){
+        if(frontier[[indexinfrontier]]$cost > current$cost + 1){
+          frontier[[indexinfrontier]] = list(pos = option, cost = current$cost + 1, prev = current)
+        }
+      } else {
+        frontier[[length(frontier)+1]] = list(pos = option, cost = current$cost + 1, prev = current)
+      }
+      
+    }
+  }
+  return (frontier)
+  
 }
 
 getTmat=function(edges){
@@ -56,8 +116,8 @@ getProbs=function(readings, probs){
     probElem = probElem * dnorm(readings[2], probs$phosphate[i,1], probs$phosphate[i,2])
     probElem = probElem * dnorm(readings[3], probs$nitrogen[i,1], probs$nitrogen[i,2])
     probVec[i] = probElem
-    sumProbs = sum(probVec)
-    probVecNormalized = probVec/sumProbs
   }
+  sumProbs = sum(probVec)
+  probVecNormalized = probVec/sumProbs
   return (probVecNormalized)
 }
